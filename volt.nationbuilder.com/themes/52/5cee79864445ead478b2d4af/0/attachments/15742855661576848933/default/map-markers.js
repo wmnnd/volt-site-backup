d2cc08d8b275c975abba503f9fd37cc8d7ab4781
Bruno Sanchez-Andrade Nuño <brunosan@gmail.com>
2020-01-18 22:44:34 +0100

    TEST hosting copy

---
+++ volt.nationbuilder.com/themes/52/5cee79864445ead478b2d4af/0/attachments/15742855661576848933/default/map-markers.js
@@ -0,0 +1,642 @@
+
+
+window.mapMarkers = [
+    /* Spain*/
+    {
+        position: [43.2630, -2.9350],
+        title: "Bilbao, Spain"
+    },
+    {
+        position: [36.7212737,-4.4213988],
+        title: "Málaga, Spain"
+    },
+    {
+        position: [38.9394272,-6.365157],
+        title: "Mérida, Spain"
+    },
+    {
+        position: [39.4699075,-0.3762881],
+        title: "Valencia, Spain"
+    },
+    {
+        position: [43.3623436,-8.4115401],
+        title: "A Coruña, Spain"
+    },
+    {
+        position: [41.3850639,2.1734035],
+        title: "Barcelona, Spain"
+    },
+    {
+        position: [40.4167754,-3.7037902],
+        title: "Madrid, Spain"
+    },
+
+
+    
+
+    /* United Kingdom */
+    {
+        position: [51.5073509,-0.1277583],
+        title: "London, United Kingdom"
+    },
+    {
+        position: [53.4807593,-2.2426305],
+        title: "Manchester, United Kingdom"
+    },
+    {
+        position: [55.953251,-3.188267],
+        title: "Edinburgh, United Kingdom"
+    },
+    {
+        position: [52.707302,-2.755327],    
+        title: "Shrewsbury, United Kingdom"
+    },
+    {
+        position: [52.486244,-1.890401],
+        title: "Birmingham, United Kingdom"
+    },
+    {
+        position: [52.281940,-1.584470],
+        title: "Warwick, United Kingdom"
+    },
+    {
+        position: [51.454514,-2.587910],
+        title: "Bristol, United Kingdom"
+    },
+    {
+        position: [52.205338,0.121817],
+        title: "Cambridge, United Kingdom"
+    },
+    {
+        position: [51.752022,-1.257726],
+        title: "Oxford, United Kingdom"
+    },
+
+
+
+    /* Poland */
+    {
+        position: [50.06465009999999,19.9449799],
+        title: "Krakow, Poland"
+    },
+    {
+        position: [52.2296756,21.0122287],
+        title: "Warsaw, Poland"
+    },
+
+
+    /* Netherlands */
+
+    {
+        position: [52.632381,4.753375],
+        title: "Alkmaar, Nederland",
+        link: "https://www.facebook.com/groups/348044895959595/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [52.3702157,4.895167900000001],
+        title: "Amsterdam, Nederland",
+        link: "https://www.facebook.com/groups/1701664126601242/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [51.851509,4.548581],
+        title: "Barendrecht, Nederland"
+    },
+    {
+        position: [51.525841,5.118750],
+        title: "Brabant, Nederland",
+        link: "https://www.facebook.com/groups/299735220877373/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [51.571915,4.768323],
+        title: "Breda, Nederland"
+    },
+    {
+        position: [52.011578,4.357068],
+        title: "Delft, Nederland",
+        link: "https://www.facebook.com/groups/648303382238599/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [52.0704978,4.3006999],
+        title: "Den Haag, Nederland",
+        link: "https://www.facebook.com/groups/381733952368995/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [52.255890,6.161370],
+        title: "Deventer, Nederland"
+    },
+    {
+        position: [51.441642,5.469722],
+        title: "Eindhoven, Nederland"
+    },
+    {
+        position: [53.2193835,6.5665017],
+        title: "Groningen, Nederland",
+        link: "https://www.voltnederland.org/groningen",
+				linkLabel: "Groningen"
+    },
+    {
+        position: [52.387386,4.646219],
+        title: "Haarlem, Nederland"
+    },
+    {
+        position: [52.229172,5.166897],
+        title: "Hilversum, Nederland",
+        link: "https://www.facebook.com/groups/320906981841069/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [52.160114,4.49701],
+        title: "Leiden, Nederland",
+        link: "https://www.facebook.com/groups/269690373689167/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [50.8513682,5.6909725],
+        title: "Maastricht, Nederland",
+        link: "https://www.facebook.com/groups/563674117426386/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [51.812565,5.837226],
+        title: "Nijmegen, Nederland",
+        link: "https://www.facebook.com/groups/292864498022395/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [51.9244201,4.4777326],
+        title: "Rotterdam, Nederland",
+        link: "https://www.facebook.com/groups/2069820579728418/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [52.09073739999999,5.1214201],
+        title: "Utrecht, Nederland",
+        link: "https://www.facebook.com/groups/2245072835505830/",
+				linkLabel: "On facebook"
+    },
+    {
+        position: [51.969189,5.665395],
+        title: "Wageningen, Nederland",
+        link: "https://www.facebook.com/groups/292864498022395/",
+				linkLabel: "On facebook"
+    },
+
+    /* Bulgaria */
+    {
+        position: [42.6977082,23.3218675],
+        title: "Sofia, Bulgaria"
+    },
+    {
+        position: [43.2140504,27.9147333],
+        title: "Varna, Bulgaria"
+    },
+    {
+        position: [43.57259,27.8272606],
+        title: "Dobrich, Bulgaria"
+    },
+    {
+        position: [43.83557130000001,25.9656554],
+        title: "Russe, Bulgaria"
+    },
+    {
+        position: [42.1927654,24.3335662],
+        title: "Pazardjik, Bulgaria"
+    },
+    {
+        position: [42.50479259999999,27.4626361],
+        title: "Bourgas, Bulgaria"
+    },
+    {
+        position: [43.2102375,23.5528803],
+        title: "Vratsa, Bulgaria"
+    },
+    {
+        position: [42.4257769,25.6344644],
+        title: "Stara Zagora, Bulgaria"
+    },
+    {
+        position: [41.5774233,24.7011138],
+        title: "Smolyan, Bulgaria"
+    },
+    {
+        position: [41.6338439,25.3777119],
+        title: "Kardjali, Bulgaria"
+    },
+    {
+        position: [43.0756739,25.6171514],
+        title: "Veliko Turnovo, Bulgaria"
+    },
+    {
+        position: [42.1354079,24.7452904],
+        title: "Plovdiv, Bulgaria"
+    },
+
+
+    /* Germany */
+    {
+        position: [52.3758916,9.732010400000002],
+        title: "Hannover, Germany"
+    },
+    {
+        position: [48.5667364,13.4319466],
+        title: "Passau, Germany"
+    },
+    {
+        position: [52.2799112,8.0471788],
+        title: "Osnabrück, Germany"
+    },
+    {
+        position: [50.1109221,8.6821267],
+        title: "Frankfurt, Germany"
+    },
+    {
+        position: [51.0504088,13.7372621],
+        title: "Dresden, Germany"
+    },
+    {
+        position: [53.5510846,9.9936819],
+        title: "Hamburg, Germany",
+        link: "https://voltdeutschland.org/hamburg",
+        linkLabel: "Read more …"
+    },
+    {
+        position: [51.2277411,6.7734556],
+        title: "Düsseldorf, Germany"
+    },
+    {
+        position: [48.1351253,11.5819805],
+        title: "Munich, Germany"
+    },
+    {
+        position: [52.52000659999999,13.404954],
+        title: "Berlin, Germany",
+        link: "https://voltdeutschland.org/berlin",
+        label: "Read more …"
+    },
+    {
+        position: [50.7753455,6.083886800000001],
+        title: "Aachen, Germany"
+    },
+    {
+        position: [50.937531,6.9602786],
+        title: "Cologne, Germany"
+    },
+    {
+        position: [52.023071,8.533210],
+        title: "Bielefeld, Germany"
+    },
+    {
+        position: [50.732700,7.096310],
+        title: "Bonn, Germany"
+    },
+    {
+        position: [47.999008,7.842104],
+        title: "Freiburg, Germany"
+    },
+    {
+        position: [51.541279,9.915804],
+        title: "Göttingen, Germany"
+    },
+    {
+        position: [49.398750,8.672434],
+        title: "Heidelberg, Germany"
+    },
+    {
+        position: [50.937531,6.960279],
+        title: "Köln, Germany"
+    },
+    {
+        position: [48.135124,11.581981],
+        title: "München, Germany"
+    },
+    {
+        position: [51.960667,7.626135],
+        title: "Münster, Germany"
+    },
+    {
+        position: [48.775845,9.182932],
+        title: "Stuttgart, Germany"
+    },
+    {
+        position: [49.452103,11.076665],
+        title: "Nürnberg, Germany"
+    },
+    {
+        position: [49.898815,10.902764],
+        title: "Bamberg, Germany"
+    },
+
+    /* belgium */
+
+    {
+        position: [50.643909,5.571560],
+        title: "Luik, Belgium"
+    },
+    {
+        position: [50.466528,4.866189],
+        title: "Namen, Belgium"
+    },
+
+    {
+        position: [50.8798438,4.7005176],
+        title: "Leuven, Belgium"
+    },
+    {
+        position: [51.0543422,3.7174243],
+        title: "Ghent, Belgium"
+    },
+    {
+        position: [50.8194776,3.2577263],
+        title: "Kortrijk, Belgium"
+    },
+    {
+        position: [51.2194475,4.4024643],
+        title: "Antwerp, Belgium"
+    },
+    {
+        position: [50.8503463,4.3517211],
+        title: "Brussels, Belgium"
+    },
+
+    /* Italy*/
+    {
+        position: [38.1156879,13.3612671],
+        title: "Palermo, Italy"
+    },
+    {
+        position: [41.1171432,16.8718715],
+        title: "Bari, Italy"
+    },
+    {
+        position: [37.5078772,15.0830304],
+        title: "Catania, Italy"
+    },
+    {
+        position: [37.5655551,14.2751913],
+        title: "Enna, Italy"
+    },
+    {
+        position: [38.90979189999999,16.5876516],
+        title: "Catanzaro, Italy"
+    },
+    {
+        position: [41.9027835,12.4963655],
+        title: "Rome, Italy"
+    },
+    {
+        position: [44.3844766,7.5426711],
+        title: "Cuneo, Italy"
+    },
+    {
+        position: [45.4642035,9.189982],
+        title: "Milan, Italy"
+    },
+    {
+        position: [44.4056499,8.946256],
+        title: "Genoa, Italy"
+    },
+    {
+        position: [45.133249,10.0226511],
+        title: "Cremona, Italy"
+    },
+    {
+        position: [44.801485,10.3279036],
+        title: "Parma, Italy"
+    },
+    {
+        position: [44.494887,11.3426162],
+        title: "Bologna, Italy"
+    },
+    {
+        position: [44.2227398,12.0407312],
+        title: "Forlì, Italy"
+    },
+    {
+        position: [43.2984268,13.4534767],
+        title: "Macerata, Italy"
+    },
+    {
+        position: [45.4064,11.8768],
+        title: "Padua, Italy"
+    },
+    {
+        position: [40.8517746,14.2681244],
+        title: "Naples, Italy"
+    },
+    {
+        position: [40.68244079999999,14.7680961],
+        title: "Salerno, Italy"
+    },
+    {
+        position: [41.5960411,14.2331612],
+        title: "Isernia, Italy"
+    },
+    {
+        position: [43.8398164,13.0194201],
+        title: "Fano, Italy"
+    },
+    {
+        position: [43.6158299,13.518915],
+        title: "Ancona, Italy"
+    },
+    {
+        position: [45.0703,7.6869],
+        title: "Torino, Italy"
+    },
+    {
+        position: [43.718030, 10.401738],
+        title: "Pisa, Italy",
+    },
+    {
+        position: [43.776187, 11.255727],
+        title: "Florence, Italy"
+    },
+
+    /*france*/
+    {
+        position: [50.62925,3.057256],
+        title: "Lille, France"
+    },
+    {
+        position: [45.764043,4.835659],
+        title: "Lyon, France"
+    },
+    {
+        position: [43.7101728,7.261953200000001],
+        title: "Nice, France"
+    },
+    {
+        position: [43.604652,1.444209],
+        title: "Toulouse, France"
+    },
+    {
+        position: [47.218371,-1.553621],
+        title: "Nantes, France"
+    },
+    {
+        position: [49.258329,4.031696],
+        title: "Reims, France"
+    },
+    {
+        position: [48.856614,2.3522219],
+        title: "Paris, France"
+    },
+    {
+        position: [48.117266,-1.6777926],
+        title: "Rennes, France"
+    },
+    {
+        position: [44.837789,-0.57918],
+        title: "Bordeaux, France"
+    },
+    {
+        position: [48.5734053,7.752111299999999],
+        title: "Strasbourg, France"
+    },
+    {
+        position: [48.757205,2.326243],
+        title: "Fresnes, France"
+    },
+    {
+        position: [43.610769,3.876716],
+        title: "Montpellier, France"
+    },
+
+    /*Sweden*/
+    {
+        position: [59.85856380000001,17.6389267],
+        title: "Uppsala, Sweden"
+    },
+    {
+        position: [59.32932349999999,18.0685808],
+        title: "Stockholm, Sweden"
+    },
+
+    /*denmark*/
+    {
+        position: [57.0488,9.9217],
+        title: "Aalborg, Denmark"
+    },
+    {
+        position: [55.6760968,12.5683372],
+        title: "Copenhagen, Denmark"
+    },
+
+    {
+        position: [56.162937,10.203921],
+        title: "Aarhus, Denmark"
+    },
+    {
+        position: [55.396229,10.390600],
+        title: "Odense, Denmark"
+    },
+
+
+    /*cyprus*/
+    {
+        position: [34.7071301,33.0226174],
+        title: "Limassol, Cyprus"
+    },
+    {
+        position: [35.1855659,33.38227639999999],
+        title: "Nicosia, Cyprus"
+    },
+
+    /*Romania*/
+    {
+        position: [44.4267674,26.1025384],
+        title: "Bucharest, Romania"
+    },
+    {
+        position: [46.7712101,23.6236353],
+        title: "Cluj-Napoca, Romania"
+    },
+
+    /* The rest */
+    {
+        position: [41.3275459,19.8186982],
+        title: "Tirana, Albania"
+    },
+    {
+        position: [54.6871555,25.2796514],
+        title: "Vilinus, Lithuania"
+    },
+    {
+        position: [53.3498053,-6.2603097],
+        title: "Dublin, Ireland"
+    },
+    {
+        position: [49.61162100000001,6.1319346],
+        title: "Luxembourg City, Luxemburg"
+    },
+    {
+        position: [56.9496,24.1052],
+        title: "Riga, Latvia"
+    },
+    {
+        position: [40.6400629,22.9444191],
+        title: "Thessaloniki, Greece"
+    },
+    {
+        position: [38.7222524,-9.1393366],
+        title: "Lisbon, Portugal"
+    },
+    {
+        position: [48.2081743,16.3738189],
+        title: "Vienna, Austria"
+    },
+    {
+      position: [48.306, 14.286],
+      title: "Linz, Austria"
+    },
+    {
+      position: [47.263, 11.400],
+      title: "Innsbruck, Austria"
+    },
+    {
+      position: [47.0709, 15.441],
+      title: "Graz, Austria"
+    },
+    {
+        position: [47.497912,19.040235],
+        title: "Budapest, Hungary"
+    },
+    {
+        position: [46.0569465,14.5057515],
+        title: "Ljubljana, Slovenia"
+    },
+    {
+        position: [60.16985569999999,24.9383791],
+        title: "Helsinki, Finland"
+    },
+    {
+        position: [45.8150108,15.9819189],
+        title: "Zagreb, Croatia"
+    },
+    {
+        position: [46.2043907,6.1431577],
+        title: "Geneva, Switzerland"
+    },
+    {
+        position: [59.43696079999999,24.7535747],
+        title: "Tallin, Estonia"
+    },
+    {
+        position: [35.8957,14.4534],
+        title: "Balzan, Malta"
+    },
+    {
+        position: [59.913868,10.752245],
+        title: "Oslo, Norway"
+    },
+
+    {
+        position: [50.0755381,14.4378005],
+        title: "Prague, Czechia"
+    }
+]
